c424a862dbb906de137c66880ffb4d355ac7b928
capt2101akash <akash.d14@iiits.in>
2016-09-28 12:33:43 +0530

    Revised 2

---
+++ docs/controller/uploadDownload.js
@@ -0,0 +1,141 @@
+var fs = require('fs'),
+    multiparty = require('multiparty'),
+    walk = require('walk'),
+    Config = require('../config/config'),
+    inert = require('inert'),
+    vision = require('vision');
+/*
+ * Display upload form
+ */
+
+exports.display_form = {
+    handler: function(request, reply) {
+        reply.file('user.html');
+    }
+};
+
+/*
+ * upload file
+ */
+
+exports.uploadFile = {
+    payload: {
+        maxBytes: 209715200,
+        output: 'stream',
+        parse: false
+    },
+    handler: function(request, reply) {
+        var form = new multiparty.Form();
+        form.parse(request.payload, function(err, fields, files) {
+            if (err) return reply(err);
+            else upload(files, reply);
+        });
+    }
+};
+
+/*
+ * upload file function
+ */
+
+var upload = function(files, reply) {
+    fs.readFile(files.file[0].path, function(err, data) {
+        checkFileExist();
+        fs.writeFile(Config.MixInsideFolder + files.file[0].originalFilename, data, function(err) {
+            if (err) return reply(err);
+            else return reply('File uploaded to: ' + Config.MixInsideFolder + files.file[0].originalFilename);
+
+        });
+    });
+};
+
+/*
+ * Check File existence and create if not exist
+ */
+
+var checkFileExist = function() {
+    fs.exists(Config.publicFolder, function(exists) {
+        if (exists === false) fs.mkdirSync(Config.publicFolder);
+
+        fs.exists(Config.MixFolder, function(exists) {
+            if (exists === false) fs.mkdirSync(Config.MixFolder);
+        });
+    });
+};
+
+/**
+ * get file
+ */
+
+exports.getFile = {
+    handler: function(request, reply) {
+        var file = request.params.file,
+            path = Config.publicFolder + Config.uploadFolder + "/" + file,
+            ext = file.substr(file.lastIndexOf('.') + 1);
+        fs.readFile(path, function(error, content) {
+            if (error) return reply("file not found");
+            var contentType;
+            switch (ext) {
+                case "pdf":
+                    contentType = 'application/pdf';
+                    break;
+                case "ppt":
+                    contentType = 'application/vnd.ms-powerpoint';
+                    break;
+                case "pptx":
+                    contentType = 'application/vnd.openxmlformats-officedocument.preplyentationml.preplyentation';
+                    break;
+                case "xls":
+                    contentType = 'application/vnd.ms-excel';
+                    break;
+                case "xlsx":
+                    contentType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+                    break;
+                case "doc":
+                    contentType = 'application/msword';
+                    break;
+                case "docx":
+                    contentType = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
+                    break;
+                case "csv":
+                    contentType = 'application/octet-stream';
+                    break;
+                default:
+                    reply.file(path);
+            }
+
+            reply(content).header('Content-Type', contentType).header("Content-Disposition", "attachment; filename=" + file);
+
+        });
+    }
+};
+
+/**
+ *get fileList
+ */
+ 
+exports.fileList = {
+    handler: function(request, reply) {
+        var files = [];
+        // Walker options
+        var walker = walk.walk(Config.publicFolder + Config.uploadFolder, {
+            followLinks: false
+        });
+
+        walker.on('file', function(root, stat, next) {
+            // Add this file to the list of files
+            files.push(stat.name);
+            next();
+        });
+        walker.on('end', function() {
+            //for(i=0;i<files.length;i++)
+            //{
+              //  if(files.length===4){
+                //console.log(end.length);
+               return reply(files + "<br />");
+               // }
+            });
+            //reply(for(i=0;i<files.length;i++){
+            //    "<ul><li>"+files[i]+"</li></ul>"
+        }
+    };
+
